812d622218726c211c9a88796ab3c96b1bb12ea0
NITISH KUMAR <81707719+nitish-09105@users.noreply.github.com>
2024-02-20 23:26:59 +0530

    applied filters

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,20 +1,66 @@
 import React, { useEffect, useState } from "react";
-import { Form, Input, Modal, Select, Table, message } from "antd";
+import { Form, Input, Modal, Select, Table, message, DatePicker } from "antd";
 import Layout from "../components/Layout/Layout";
-
 import Spinner from "../components/Spinner";
+import moment from "moment";
 import axios from "axios";
+
+const { RangePicker } = DatePicker;
 
 const HomePage = () => {
   const [showModal, setShowModal] = useState(false);
   const [loading, setLoading] = useState(false);
   const [allTransaction, setAllTransaction] = useState([]);
+  const [frequency, setFrequency] = useState("7");
+  const [selectedDate, setSelectedDate] = useState([]);
+  const [type, setType] = useState("all");
 
-  // tableData
+  useEffect(() => {
+    const getAllTransaction = async () => {
+      try {
+        const user = JSON.parse(localStorage.getItem("user"));
+        setLoading(true);
+        const res = await axios.post("/transactions/get-transaction", {
+          userid: user._id,
+          frequency,
+          selectedDate,
+          type,
+        });
+
+        setAllTransaction(res.data);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        console.log(error);
+        message.error("Fetch issue with transaction");
+      }
+    };
+    getAllTransaction();
+  }, [frequency, selectedDate,type]);
+
+  const handleSubmit = async (values) => {
+    try {
+      const user = JSON.parse(localStorage.getItem("user"));
+      setLoading(true);
+      await axios.post("/transactions/add-transaction", {
+        ...values,
+        userid: user._id,
+      });
+
+      message.success("Transaction added successfully");
+      setShowModal(false);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      message.error("Failed to add Transactions");
+    }
+  };
+
   const columns = [
     {
       title: "Date",
       dataIndex: "date",
+      render: (text) => <span>{moment(text).format("YYYY-MM-DD")}</span>,
     },
     {
       title: "Amount",
@@ -36,50 +82,39 @@ const HomePage = () => {
       title: "Actions",
     },
   ];
-  // get all transactions
-  const getAllTransaction = async () => {
-    try {
-      const user = JSON.parse(localStorage.getItem("user"));
-      setLoading(true);
-      const res = await axios.post("/transactions/get-transaction", {
-        userid: user._id,
-      });
-      setLoading(false);
-      setAllTransaction(res.data);
-      console.log(res.data);
-    } catch (error) {
-      console.log(error);
-      message.error("Fetch issue with transaction");
-    }
-  };
 
-  // useeffect
-  useEffect(() => {
-    getAllTransaction();
-  }, []);
-
-  // form handling
-  const handleSubmit = async (values) => {
-    try {
-      const user = JSON.parse(localStorage.getItem("user"));
-      setLoading(true);
-      await axios.post("/transactions/add-transaction", {
-        ...values,
-        userid: user._id,
-      });
-
-      message.success("Transaction added successfully");
-      setShowModal(false);
-    } catch (error) {
-      setLoading(false);
-      message.error("Failed to add Transactions");
-    }
-  };
   return (
-    <Layout>
-      {loading && <Spinner />}
+    <Layout loading={loading}>
       <div className="filters">
-        <div>range filters</div>
+        <div>
+          <h6>Select Frequency</h6>
+          <Select value={frequency} onChange={(value) => setFrequency(value)}>
+            <Select.Option value="7">Last 1 week</Select.Option>
+            <Select.Option value="30">Last 1 Month</Select.Option>
+            <Select.Option value="365">Last 1 Year</Select.Option>
+            <Select.Option value="custom">Custom</Select.Option>
+          </Select>
+          {frequency === "custom" && (
+            <RangePicker
+              value={selectedDate}
+              onChange={(dates) => setSelectedDate(dates)}
+            />
+          )}
+        </div>
+        <div>
+          <h6>Select Type</h6>
+          <Select value={type} onChange={(value) => setType(value)}>
+            <Select.Option value="all"> ALL</Select.Option>
+            <Select.Option value="income">INCOME</Select.Option>
+            <Select.Option value="expense">EXPENSE</Select.Option>
+          </Select>
+          {frequency === "custom" && (
+            <RangePicker
+              value={selectedDate}
+              onChange={(dates) => setSelectedDate(dates)}
+            />
+          )}
+        </div>
         <div>
           <button
             className="btn btn-primary"
@@ -97,7 +132,7 @@ const HomePage = () => {
         title="Add Transaction"
         open={showModal}
         onCancel={() => setShowModal(false)}
-        footer={false}
+        footer={null}
       >
         <Form layout="vertical" onFinish={handleSubmit}>
           <Form.Item label="Amount" name="amount">
@@ -111,7 +146,9 @@ const HomePage = () => {
           </Form.Item>
           <Form.Item label="Category" name="category">
             <Select>
-              <Select.Option value="salary">Salary</Select.Option>
+              <Select.Option ption value="salary">
+                Salary
+              </Select.Option>
               <Select.Option value="tip">Tip</Select.Option>
               <Select.Option value="project">Project</Select.Option>
               <Select.Option value="food">Food</Select.Option>
@@ -121,7 +158,7 @@ const HomePage = () => {
             </Select>
           </Form.Item>
           <Form.Item label="Date" name="date">
-            <Input type="date" />
+            <DatePicker />
           </Form.Item>
           <Form.Item label="Reference" name="reference">
             <Input type="text" />
--- server/models/transactionModel.js
+++ server/models/transactionModel.js
@@ -26,7 +26,7 @@ const transactionSchema = new mongoose.Schema(
       required: [true, "Description is required"],
     },
     date: {
-      type: String,
+      type: Date,
       required: [true, "Date is required"],
     },
   },
